6e0ab6b3d98aceb8755db1e446fcf990922f2a8f
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2022-09-05 22:15:22 +0700

    config router and layout for project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRouters } from '~/routers/index.js';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Học ReactJS</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRouters.map((router, index) => {
+                        const Page = router.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (router.layout === null) {
+                            Layout = Fragment;
+                        } else if (router.layout) {
+                            Layout = router.layout;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={router.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            ></Route>
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/routers/index.js
@@ -0,0 +1,19 @@
+// import Layout
+import { HeaderOnly } from '~/components/Layout';
+
+// import Component
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+const publicRouters = [
+    { path: '/', component: Home },
+    { path: 'following', component: Following },
+    { path: 'profile', component: Profile },
+    { path: 'upload', component: Upload, layout: HeaderOnly },
+];
+
+const privvateRouters = [];
+
+export { publicRouters, privvateRouters };
