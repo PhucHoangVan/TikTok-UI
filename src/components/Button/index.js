b65ce1f08217822444e7ecc27f7a8d36dc7b8415
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2022-09-08 22:41:40 +0700

    Create Header part-3
    <Create Component Button>
    - for three case: onClick for btn, to for Link, href for tag a.
    - for three size: small, medium, large.
    - for type: primary, rounded, outline, text.
    - Add previous icon, suffix icon for Button.

---
+++ src/components/Button/index.js
@@ -0,0 +1,68 @@
+import styles from './Button.module.scss';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    children,
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    rounded = false,
+    disable,
+    small = false,
+    large = false,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    // Remove event when button disable
+    if (disable) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        Comp = Link;
+        props.to = to;
+    } else if (href) {
+        Comp = 'a';
+        props.href = href;
+    }
+
+    const classes = cx('wrapper', {
+        primary,
+        outline,
+        text,
+        rounded,
+        disable,
+        small,
+        large,
+        [className]: className,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faPlus } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import { useEffect, useState } from 'react';
@@ -8,6 +8,7 @@ import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Accountitem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 const cx = classNames.bind(styles);
 
@@ -22,33 +23,42 @@ function Header() {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok"></img>
-                <Tippy
-                    interactive
-                    visible={searchHistory.length > 0}
-                    render={(attrs) => (
-                        <div className={cx('search-history')} tabIndex="-1" {...attrs}>
-                            <PopperWrapper>
-                                <h4 className={cx('search-title')}>Accounts</h4>
-                                <Accountitem />
-                                <Accountitem />
-                                <Accountitem />
-                                <Accountitem />
-                            </PopperWrapper>
+                <div>
+                    <Tippy
+                        interactive
+                        visible={searchHistory.length > 0}
+                        render={(attrs) => (
+                            <div className={cx('search-history')} tabIndex="-1" {...attrs}>
+                                <PopperWrapper>
+                                    <h4 className={cx('search-title')}>Accounts</h4>
+                                    <Accountitem />
+                                    <Accountitem />
+                                    <Accountitem />
+                                    <Accountitem />
+                                </PopperWrapper>
+                            </div>
+                        )}
+                    >
+                        <div className={cx('search')}>
+                            <input placeholder="Search account or videos" spellCheck={false} />
+                            <button>
+                                <FontAwesomeIcon className={cx('clear')} icon={faCircleXmark} />
+                            </button>
+                            {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                            <button className={cx('search-btn')}>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </button>
                         </div>
-                    )}
-                >
-                    <div className={cx('search')}>
-                        <input placeholder="Search account or videos" spellCheck={false} />
-                        <button>
-                            <FontAwesomeIcon className={cx('clear')} icon={faCircleXmark} />
-                        </button>
-                        {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
-                        <button className={cx('search-btn')}>
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />
-                        </button>
-                    </div>
-                </Tippy>
-                <div className={cx('action')}></div>
+                    </Tippy>
+                </div>
+                <div className={cx('action')}>
+                    <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>
+                        Upload
+                    </Button>
+                    <Button className={cx('custom-btn')} primary>
+                        Log in
+                    </Button>
+                </div>
             </div>
         </header>
     );
